7216196086fa7af016d320c498be3583ac44b19e
Daniel Kong <dakong94@gmail.com>
2017-01-26 19:52:00 -0800

    added static home site

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,6 @@ from .models import Post
 
 
 # Create your views here.
-
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
@@ -41,4 +40,3 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
- 
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
+	url(r'^$', views.home_page, name='home_page'),
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
